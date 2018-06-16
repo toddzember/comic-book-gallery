a3f116791cd5cbd76454c47ad34fc737a1bfa391
ToddZember <toddzember@gmail.com>
2018-06-16 08:50:58 -0400

    Added Comic Book Detail view.

--- src/ComibBookGallery/Controllers/ComicBookController.cs
+++ src/ComibBookGallery/Controllers/ComicBookController.cs
@@ -10,17 +10,7 @@ namespace ComibBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-
-            if (DateTime.Today.DayOfWeek==DayOfWeek.Wednesday)
-                {
-                return Redirect("/");
-
-            }
-
-           
-              return  Content("Hello from the comic books controller!");
-
-           
+            return View();
         }
     }
 
